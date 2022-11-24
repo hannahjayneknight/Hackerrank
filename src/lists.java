6b7a9771cbfce06110a55bff8f7e7f6479726760
Hannah Jayne Knight <hannah@tinpot.co.uk>
2022-11-24 18:41:29 +0000

    first commit

---
+++ src/lists.java
@@ -0,0 +1,20 @@
+
+public class lists {
+
+	public static void main(String[] args) {
+		// Advantage of ArrayList over list = can change size dynamically
+		
+		// sorting a list
+		
+		// creating a list full of zeros
+		
+		// looping through a list
+		
+		// finding the size of a list
+		
+		
+		// NESTED LISTS
+
+	}
+
+}
